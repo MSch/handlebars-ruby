81232d22aee54974cc7108e6ccf6616b9d06db74
Martin Schürrer <martin@schuerrer.org>
2010-09-10 06:55:40 +0200

    Implementing add_text and parts of parse_mustache

--- lib/handlebars/compiler.rb
+++ lib/handlebars/compiler.rb
@@ -2,23 +2,53 @@ require 'stringio'
 
 class Handlebars
   class Compiler
-    attr_accessor :string, :pointer, :mustache, :text, :fn, :newlines, :comment, :escaped, :partial, :inverted, :end_condition, :continue_inverted
-    def initialize(string=StringIO.new(''))
-        @string   = string
-        @pointer  = -1
-        @mustache = false
-        @text     = ''
-        @fn       = 'out = ""; lookup = nil; '
-        @newlines = ''
-        @comment  = false
-        @escaped  = true
-        @partial  = false
-        @inverted = false
-        @end_condition = nil
-        @continue_inverted = false
+    attr_accessor :input, :pointer, :mustache, :text, :fn, :newlines, :comment, :escaped, :partial, :inverted, :end_condition, :continue_inverted
+    def initialize(input)
+      @input   = input
+      @pointer  = -1
+      @mustache = false
+      @text     = ''
+      @fn       = 'out = ""; lookup = nil; '
+      @newlines = ''
+      @comment  = false
+      @escaped  = true
+      @partial  = false
+      @inverted = false
+      @end_condition = nil
+      @continue_inverted = false
+    end
+
+    def add_text
+     if not @text.empty?
+       @fn << 'out = out + \'' + CGI.escapeHTML(@text) + '\''
+       @fn << @newlines
+       @newlines = ''
+       @text = ''
+     end
     end
 
     def parse_mustache
+      chr, part, mustache, param = nil
+      @s = StringScanner.new(input)
+
+      next_char = @s.peek
+      case next_char
+      when '!'
+        @comment = true
+      when '#'
+        @open_block = true
+      when '>'
+        @partial = true
+      when '^'
+        @inverted = true
+        @open_block = true
+      when '{'
+        @escaped = false
+      when '&'
+        @escaped = false
+      end
+      @s.getch
+
 
     end
 
