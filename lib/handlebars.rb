0a71c561b4cf29b30a8a886ab426d95b362d3746
Martin Schürrer <martin@schuerrer.org>
2010-09-10 06:29:50 +0200

    escape_text

--- lib/handlebars.rb
+++ lib/handlebars.rb
@@ -1,4 +1,5 @@
 class Handlebars
+  attr_accessor :compiled_partials
   def compile(input)
   end
 
@@ -8,13 +9,13 @@ class Handlebars
   def compile_function_body(input)
   end
 
-  def escape_text
+  def escape_text(input)
+    CGI.escapeHTML(input)
   end
 
-  def escape_expression
+  def escape_expression(input)
   end
 
-  attr_accessor :compiled_partials
 
   def compile_partials(input)
   end
